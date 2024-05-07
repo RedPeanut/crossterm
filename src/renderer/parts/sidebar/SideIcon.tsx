ee1ca30889c007b759784c14621508402aa7611f
김진규 <zamong99@gmail.com>
2024-05-07 22:02:51 +0900

    add sidebar active icon indicator

--- src/renderer/parts/sidebar/SideIcon.tsx
+++ src/renderer/parts/sidebar/SideIcon.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classnames from 'classnames';
+import { connect } from 'react-redux';
 
 // const { prefix } = window;
 // console.log('prefix =', prefix);
@@ -9,12 +10,15 @@ interface SideIconProps {
   title?: string;
   children?: React.ReactElement | React.ReactElement[];
   onClick?: React.MouseEventHandler<HTMLDivElement> | undefined;
+
+  // mapped value
+  sidePanel: any;
 }
 
 function SideIcon(props: SideIconProps) {
-  const { className, title, children } = props;
+  const { className, title, children, sidePanel } = props;
 
-  const cls = classnames(className, 'control-icon-wrap', {});
+  const cls = classnames(className, 'control-icon-wrap', sidePanel.active === title ? 'checked' : null);
   const onClickOne: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
     = (e) => {}
 
@@ -22,21 +26,21 @@ function SideIcon(props: SideIconProps) {
     <div className={cls} title={title}
       onClick={props.onClick ? props.onClick : onClickOne}
     >
-      {children}
-      {/* <div className="badge"></div>
-      <div className="active-item-indicator"></div> */}
+      { children }
+      {/* <div className="badge"></div> */}
+      <div className='active-item-indicator'></div>
     </div>
   );
 }
 
-// SideIcon.propTypes = {
-//   className: PropTypes.string,
-//   title: PropTypes.string,
-// };
+const mapStateToProps = (state: any) => {
+  return {
+    sidePanel: state.app.sidePanel,
+  };
+};
 
-// SideIcon.defaultProps = {
-//   className: '',
-//   title: '',
-// };
+const mapDispatchToProps = (dispatch: any) => {
+  return {};
+};
 
-export default SideIcon;
+export default connect(mapStateToProps, mapDispatchToProps)(SideIcon);
