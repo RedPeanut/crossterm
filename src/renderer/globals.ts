f3b8d5975ac5577cf4854c177feb51c727af9c85
김진규 <zamong99@gmail.com>
2024-05-27 15:07:10 +0900

    일단 글로벌 변수에 꼽아둠

--- src/renderer/globals.ts
+++ src/renderer/globals.ts
@@ -1,1 +1,7 @@
+import { I18N, LocaleName } from "../common/i18n";
+
+let _locale = localStorage.getItem('locale') as LocaleName | undefined;
+export const locale = _locale || 'en';
+export const i18n = new I18N(locale);
+export const lang = i18n.lang;
 export const terminals: { [key: string]: any } = {};
