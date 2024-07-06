3729a58041c6797aea9f08108ec39106be7bea9a
김진규 <zamong99@gmail.com>
2024-07-06 11:40:17 +0900

    코드정리

--- src/renderer/parts/list/List.tsx
+++ src/renderer/parts/list/List.tsx
@@ -30,45 +30,45 @@ class List extends React.Component<ListProps, ListState> {
       selected_ids: ['0'],
       show_list: [
         {
-          type: "folder",
-          title: "folder",
-          id: "52528ee3-aa4f-44a5-b763-5cf69acacf51",
+          type: 'folder',
+          title: 'folder',
+          id: '52528ee3-aa4f-44a5-b763-5cf69acacf51',
           on: false,
           children: [
             {
-              title: "xyz",
-              id: "e54af9c1-f003-4b1b-8db4-e796f69a9a4d",
+              title: 'xyz',
+              id: 'e54af9c1-f003-4b1b-8db4-e796f69a9a4d',
               on: true
             },
             {
-              title: "title1",
-              id: "96367ed9-6fb1-434b-b45d-de9d2d21898a",
+              title: 'title1',
+              id: '96367ed9-6fb1-434b-b45d-de9d2d21898a',
               on: true
             },
             {
-              title: "title2",
-              id: "9a136351-e893-4be6-a931-1b6a5ce85031",
+              title: 'title2',
+              id: '9a136351-e893-4be6-a931-1b6a5ce85031',
               on: true
             }
           ],
-          "is_collapsed": false
+          'is_collapsed': false
         },
         {
-          type: "remote",
-          title: "remote",
-          url: "www.remote.com",
-          id: "8d65f5a3-306d-44c7-a43f-b5abc17b6a2b",
+          type: 'remote',
+          title: 'remote',
+          url: 'www.remote.com',
+          id: '8d65f5a3-306d-44c7-a43f-b5abc17b6a2b',
           on: true
         },
         {
-          type: "group",
-          title: "group",
-          id: "cbf8ea19-4474-4c15-8af0-3a4bdcdff717"
+          type: 'group',
+          title: 'group',
+          id: 'cbf8ea19-4474-4c15-8af0-3a4bdcdff717'
         },
         {
-          type: "local",
-          title: "local",
-          id: "751b26d0-5c94-4328-a0e8-23fdd85d160f"
+          type: 'local',
+          title: 'local',
+          id: '751b26d0-5c94-4328-a0e8-23fdd85d160f'
         }
       ]
     };
