0f7f45f45b498a3c7aa5031027a795726e6e9a08
김진규 <zamong99@gmail.com>
2025-01-04 22:49:55 +0900

    add module

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,1 +1,17 @@
 console.log('👋 This message is being logged by "renderer", included via webpack');
+
+import '@vscode/codicons/dist/codicon.css';
+import './index.css';
+
+export type CodeWindow = Window & typeof globalThis;
+export const mainWindow = window as CodeWindow;
+
+export class Renderer {
+  constructor() {}
+
+  async open() {
+  }
+}
+
+const renderer = new Renderer();
+renderer.open();
