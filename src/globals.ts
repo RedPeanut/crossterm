ec4848e6913358a07474bf7fa653049de50dab23
김진규 <zamong99@gmail.com>
2025-01-29 23:35:52 +0900

    add split-view divider

--- src/globals.ts
+++ src/globals.ts
@@ -12,23 +12,20 @@ export const wrapper: { [key: string]: SplitItem } = {
       [{uid:'a1',selected:false},{uid:'a2',selected:true,active:true}]
     ]
   } //*/
-  ///* case2. single split
+  /* case2. single split
   'tree': {
-    // mode: 'horizontal',
     mode: 'vertical',
     list:[
       [{uid:'a1',selected:false},{uid:'a2',selected:true,active:true}],
       [{uid:'b1',selected:true}]
     ]
   } //*/
-  /* case2-1.
+  ///* case2-1. single split
   'tree': {
-    // mode: 'horizontal',
-    mode: 'vertical',
+    mode: 'horizontal',
     list:[
-      [{uid:'a1',selected:true}],
-      [{uid:'a2',selected:true,active:true}],
-      [{uid:'a3',selected:true}]
+      [{uid:'a1',selected:true},{uid:'a2',selected:true},{uid:'a3',selected:true,active:true}],
+      [{uid:'b1',selected:true}]
     ]
   } //*/
   /* case3. split vertical in right pane
