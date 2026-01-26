95792ef4f8f6e168ebc47be1cae51ddbf220f7af
김진규 <zamong99@gmail.com>
2026-01-26 22:06:48 +0900

    change main.ts to class (encaptualization)

--- src/main/main.ts
+++ src/main/main.ts
@@ -27,154 +27,162 @@ class AppUpdater {
   }
 }
 
-let mainWindow: BrowserWindow | null = null;
+class MainWindow {
+  browserWindow: BrowserWindow | null = null;
+  isDebug: boolean = false;
 
-if (process.env.NODE_ENV === 'production') {
-  const sourceMapSupport = require('source-map-support');
-  sourceMapSupport.install();
-}
-
-const isDebug =
-  process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true';
-
-if (isDebug) {
-  require('electron-debug')();
-}
-
-const installExtensions = async () => {
-  const installer = require('electron-devtools-installer');
-  const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
-  const extensions = ['REACT_DEVELOPER_TOOLS'];
-
-  return installer
-    .default(
-      extensions.map((name) => installer[name]),
-      forceDownload,
-    )
-    .catch(console.log);
-};
-
-const getMaxScreenSize = () => {
-  let rect = { x: -1, y: -1, height: -1, width: -1 };
-  const _screen = screen.getDisplayMatching(rect);
-  return {
-    ..._screen.workArea
-  }
-}
-
-const getWindowSize = () => {
-
-  // todo: if dev maxmize window
-  // else if exist last state window size then use
-  // else set to default size (width: 1024, height: 728)
-
-  const {
-    width: maxWidth,
-    height: maxHeight
-  } = getMaxScreenSize();
-
-  return {
-    x: 0, y: 0,
-    width: maxWidth,
-    height: maxHeight
-  }
-}
-
-const installIpc = () => {
-  const terminals = new Map<string, TerminalBase>();
-
-  ipcMain.on('new', (event, args: any[]) => {
-    // console.log('[main.ts/new] args =', args);
-    const arg: TerminalItem = args[0] as TerminalItem;
-    if(arg.type === 'local') {
-      const terminal = new TerminalLocal(arg);
-      terminal.on('data', (data: string) => {
-        // console.log('data event is called..., data =', data);
-        mainWindow?.webContents.send('terminal data', data);
-      });
-      terminal.start();
-      terminals.set(arg.uid, terminal);
-    } else if(arg.type === 'remote') {
-      const terminal = new TerminalSsh(arg);
-      terminal.on('data', (data: string) => {
-        // console.log('data event is called..., data =', data);
-        mainWindow?.webContents.send('terminal data', data);
-      });
-      terminal.start();
-      terminals.set(arg.uid, terminal);
+  constructor() {
+    if(process.env.NODE_ENV === 'production') {
+      const sourceMapSupport = require('source-map-support');
+      sourceMapSupport.install();
     }
-  });
 
-  ipcMain.on('data', (event, args: any[]) => {
-    // console.log('[main.ts/data] args =', args);
-    const arg = args[0];
-    const terminal = arg && arg.uid && terminals.get(arg.uid);
-    if(terminal) {
-      terminal.write(arg.data);
+    this.isDebug =
+      process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true';
+
+    if(this.isDebug) {
+      require('electron-debug')();
     }
-  });
-}
-
-const createWindow = async () => {
-  if (isDebug) {
-    await installExtensions();
   }
 
-  const RESOURCES_PATH = app.isPackaged
-    ? path.join(process.resourcesPath, 'assets')
-    : path.join(__dirname, '../../assets');
+  installExtensions = async () => {
+    const installer = require('electron-devtools-installer');
+    const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
+    const extensions = ['REACT_DEVELOPER_TOOLS'];
 
-  const getAssetPath = (...paths: string[]): string => {
-    return path.join(RESOURCES_PATH, ...paths);
+    return installer
+      .default(
+        extensions.map((name) => installer[name]),
+        forceDownload,
+      )
+      .catch(console.log);
   };
 
-  const { x, y, width, height } = getWindowSize();
-
-  mainWindow = new BrowserWindow({
-    // show: false,
-    // width: 1024, height: 728,
-    titleBarStyle: 'hidden',
-    x, y, width, height,
-    icon: getAssetPath('icon.png'),
-    webPreferences: {
-      preload: app.isPackaged
-        ? path.join(__dirname, 'preload.js')
-        : path.join(__dirname, '../../.erb/dll/preload.js'),
-    },
-  });
-
-  installIpc();
-
-  mainWindow.loadURL(resolveHtmlPath('index.html'));
-
-  mainWindow.on('ready-to-show', () => {
-    if (!mainWindow) {
-      throw new Error('"mainWindow" is not defined');
+  getMaxScreenSize = () => {
+    let rect = { x: -1, y: -1, height: -1, width: -1 };
+    const _screen = screen.getDisplayMatching(rect);
+    return {
+      ..._screen.workArea
     }
-    if (process.env.START_MINIMIZED) {
-      mainWindow.minimize();
-    } else {
-      mainWindow.show();
+  }
+
+  getWindowSize = () => {
+
+    // todo: if dev maxmize window
+    // else if exist last state window size then use
+    // else set to default size (width: 1024, height: 728)
+
+    const {
+      width: maxWidth,
+      height: maxHeight
+    } = this.getMaxScreenSize();
+
+    return {
+      x: 0, y: 0,
+      width: maxWidth,
+      height: maxHeight
     }
-  });
+  }
 
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
+  installIpc = () => {
+    const self = this;
 
-  const menuBuilder = new MenuBuilder(mainWindow);
-  menuBuilder.buildMenu();
+    const terminals = new Map<string, TerminalBase>();
 
-  // Open urls in the user's browser
-  mainWindow.webContents.setWindowOpenHandler((edata) => {
-    shell.openExternal(edata.url);
-    return { action: 'deny' };
-  });
+    ipcMain.on('new', (event, args: any[]) => {
+      // console.log('[main.ts/new] args =', args);
+      const arg: TerminalItem = args[0] as TerminalItem;
+      if(arg.type === 'local') {
+        const terminal = new TerminalLocal(arg);
+        terminal.on('data', (data: string) => {
+          // console.log('data event is called..., data =', data);
+          this.browserWindow?.webContents.send('terminal data', data);
+        });
+        terminal.start();
+        terminals.set(arg.uid, terminal);
+      } else if(arg.type === 'remote') {
+        const terminal = new TerminalSsh(arg);
+        terminal.on('data', (data: string) => {
+          // console.log('data event is called..., data =', data);
+          this.browserWindow?.webContents.send('terminal data', data);
+        });
+        terminal.start();
+        terminals.set(arg.uid, terminal);
+      }
+    });
 
-  // Remove this if your app does not use auto updates
-  // eslint-disable-next-line
-  new AppUpdater();
-};
+    ipcMain.on('data', (event, args: any[]) => {
+      // console.log('[main.ts/data] args =', args);
+      const arg = args[0];
+      const terminal = arg && arg.uid && terminals.get(arg.uid);
+      if(terminal) {
+        terminal.write(arg.data);
+      }
+    });
+  }
+
+  createWindow = async () => {
+    if(this.isDebug) {
+      await this.installExtensions();
+    }
+
+    const RESOURCES_PATH = app.isPackaged
+      ? path.join(process.resourcesPath, 'assets')
+      : path.join(__dirname, '../../assets');
+
+    const getAssetPath = (...paths: string[]): string => {
+      return path.join(RESOURCES_PATH, ...paths);
+    };
+
+    const { x, y, width, height } = this.getWindowSize();
+
+    this.browserWindow = new BrowserWindow({
+      // show: false,
+      // width: 1024, height: 728,
+      titleBarStyle: 'hidden',
+      x, y, width, height,
+      icon: getAssetPath('icon.png'),
+      webPreferences: {
+        preload: app.isPackaged
+          ? path.join(__dirname, 'preload.js')
+          : path.join(__dirname, '../../.erb/dll/preload.js'),
+      },
+    });
+
+    this.browserWindow.loadURL(resolveHtmlPath('index.html'));
+
+    this.installIpc();
+
+    this.browserWindow.on('ready-to-show', () => {
+      if(!this.browserWindow) {
+        throw new Error('"mainWindow" is not defined');
+      }
+      if(process.env.START_MINIMIZED) {
+        this.browserWindow.minimize();
+      } else {
+        this.browserWindow.show();
+      }
+    });
+
+    this.browserWindow.on('closed', () => {
+      this.browserWindow = null;
+    });
+
+    const menuBuilder = new MenuBuilder(this.browserWindow);
+    menuBuilder.buildMenu();
+
+    // Open urls in the user's browser
+    this.browserWindow.webContents.setWindowOpenHandler((edata) => {
+      shell.openExternal(edata.url);
+      return { action: 'deny' };
+    });
+
+    // Remove this if your app does not use auto updates
+    // eslint-disable-next-line
+    new AppUpdater();
+  };
+}
+
 
 /**
  * Add event listeners...
@@ -183,19 +191,21 @@ const createWindow = async () => {
 app.on('window-all-closed', () => {
   // Respect the OSX convention of having the application in memory even
   // after all windows have been closed
-  if (process.platform !== 'darwin') {
+  if(process.platform !== 'darwin') {
     app.quit();
   }
 });
 
+export const mainWindow = new MainWindow();
+
 app
   .whenReady()
   .then(() => {
-    createWindow();
+    mainWindow.createWindow();
     app.on('activate', () => {
       // On macOS it's common to re-create a window in the app when the
       // dock icon is clicked and there are no other windows open.
-      if (mainWindow === null) createWindow();
+      // TODO: if(mainWindow === null) mainWindow.createWindow();
     });
   })
   .catch(console.log);
