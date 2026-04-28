0178e67536109bceab6d04a5344d5cb3f596c8ac
김진규 <zamong99@gmail.com>
2026-04-28 21:56:09 +0900

    add test code template

---
+++ test/sample.test.ts
@@ -0,0 +1,8 @@
+// import fs from 'fs';
+
+describe('#sample', function() {
+  it('default ', function() {
+
+  });
+
+});
---
+++ test/something.test.ts
@@ -0,0 +1,8 @@
+import { v4 as uuidv4 } from 'uuid';
+
+describe('#something', function() {
+  it('default ', function() {
+    console.log(uuidv4());
+  });
+
+});
