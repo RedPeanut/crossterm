0a8a519fcefed319b9cec9f513ef87775e9ae4ee
김진규 <zamong99@gmail.com>
2026-05-07 21:36:00 +0900

    impl Disposable

--- src/renderer/Lifecycle.ts
+++ src/renderer/Lifecycle.ts
@@ -1,9 +1,52 @@
-export interface IDisposable {
-  dispose(): void;
-}
+import { Channels, ElectronHandler } from "../main/preload";
 
-export abstract class Disposable implements IDisposable {
+// type Who;
+type Where = HTMLElement | ElectronHandler;
+// type What;
+// type Handler = (() => void) | ((...args: any[]) => void);
+
+export class Disposable {
+
+  // map: Map<string, (e: Event) => void> = new Map();
+  map: Map<Where, Map<string, ((e: Event) => void)[]>> = new Map();
+
   dispose(): void {
-    throw new Error("Method not implemented.");
+    for(const [where, what] of this.map) {
+      for(const [event, handlers] of this.map.get(where)) {
+        for(const handler of handlers) {
+          if(where instanceof HTMLElement)
+            where.removeEventListener(event, handler);
+          // else if(where instanceof ElectronHandler) // 'ElectronHandler' only refers to a type, but is being used as a value here.ts(2693)
+          // else if(typeof where === typeof window.ipc)
+          else if(where === window.ipc)
+            where.off(event as Channels, handler);
+        }
+      }
+      this.map.get(where).clear();
+    }
   }
+
+  register(where: Where, event: string, handler: (e: Event) => void) {
+    if(where instanceof HTMLElement)
+      where.addEventListener(event, handler);
+    else if(where === window.ipc) {
+      where.on(event as Channels, handler);
+    } else {
+      // must do not enter here
+      throw new Error();
+    }
+
+    if(this.map.get(where)) {
+      if(this.map.get(where).get(event)) {
+        this.map.get(where).get(event).push(handler);
+      } else {
+        this.map.get(where).set(event, [ handler ]);
+      }
+    } else {
+      const what = new Map<string, any>();
+      what.set(event, [ handler ]);
+      this.map.set(where, what);
+    }
+  }
+
 }
