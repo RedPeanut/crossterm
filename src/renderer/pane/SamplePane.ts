4fdb8820eedae7cbc44454e60ef2f6052f882adf
김진규 <zamong99@gmail.com>
2026-05-23 19:51:21 +0900

    샘플 코드 업데이트

--- src/renderer/pane/SamplePane.ts
+++ src/renderer/pane/SamplePane.ts
@@ -1,13 +1,38 @@
-import { Pane } from "../Pane";
+import { Pane, PaneOptions } from "../Pane";
+import { getService, sidebarPartServiceId } from "../Service";
+import { SidebarPartService } from "../part/SidebarPart";
+import { $ } from "../util/dom";
 
 export class SamplePane extends Pane {
 
+  override layout(offset: number, size: number) {
+    // console.log(`layout is called .., offset = ${offset}, size = ${size}`);
+  }
+
+  constructor(parent: HTMLElement, options: PaneOptions) {
+    super(parent, options);
+    this.element.classList.add('detail');
+    this.minimumSize = Pane.HEADER_SIZE;
+  }
+
   renderHeader(container: HTMLElement): void {
-    throw new Error("Method not implemented.");
+    const arrow = $('.arrow');
+    const right = $('a.codicon.codicon-chevron-right');
+    arrow.appendChild(right);
+    arrow.addEventListener('click', (e: MouseEvent) => {
+      this.expanded = !this.expanded;
+      (getService(sidebarPartServiceId) as SidebarPartService).layout(null, null);
+    });
+    this.header.appendChild(arrow);
+    const title = $('h3.title');
+    title.innerHTML = 'SAMPLE';
+    this.header.appendChild(title);
   }
 
   renderBody(container: HTMLElement): void {
-    throw new Error("Method not implemented.");
+    const p = $('p');
+    p.innerHTML = 'blarblarblar<br/>blarblarblar<br/>blarblarblar';
+    this.body.appendChild(p);
   }
 
 }
--- src/renderer/paneView/SamplePaneView.ts
+++ src/renderer/paneView/SamplePaneView.ts
@@ -1,7 +1,8 @@
 import { PaneView } from "../PaneView";
+import { SamplePane } from "../pane/SamplePane";
 
 export class SamplePaneView extends PaneView {
-  static ID = 'pane-container.sample';
+  static ID = 'pane-view.sample';
 
   constructor() {
     super({ id: SamplePaneView.ID });
@@ -10,5 +11,9 @@ export class SamplePaneView extends PaneView {
   override create(parent: HTMLElement): void {
     super.create(parent);
     this.element.classList.add('sample');
+
+    const samplePane = new SamplePane(null, {});
+    samplePane.render();
+    this.addPane(samplePane);
   }
 }
