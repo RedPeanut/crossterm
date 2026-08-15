83221c2e7dcdc578c36a03d067cc7db2c216f546
김진규 <zamong99@gmail.com>
2026-08-15 17:18:04 +0900

    .

--- src/test/sample.test.ts
+++ src/test/sample.test.ts
@@ -1,8 +1,11 @@
+import assert from 'assert';
 // import fs from 'fs';
 
 describe('#sample', function() {
   it('default', function() {
-
+    // assert.equal(received, expected); // 느슨한 비교 (사용 지양)
+    // assert.strictEqual(received, expected); // 원시값 비교
+    // assert.deepStrictEqual(received, expected); // 객체/배열 구조 비교
   });
 
 });
