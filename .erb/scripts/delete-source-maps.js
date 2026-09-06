e2ac35a7aee6084fef1073614c474204e1b4ce1b
김진규 <zamong99@gmail.com>
2026-09-06 14:45:22 +0900

    fix npm run package

--- .erb/scripts/delete-source-maps.js
+++ .erb/scripts/delete-source-maps.js
@@ -1,15 +1,17 @@
 import fs from 'fs';
 import path from 'path';
-import { rimrafSync } from 'rimraf';
 import webpackPaths from '../configs/webpack.paths';
 
+function deleteSourceMapsIn(dirPath) {
+  if (!fs.existsSync(dirPath)) return;
+  fs.readdirSync(dirPath)
+    .filter((fileName) => fileName.endsWith('.js.map'))
+    .forEach((fileName) => {
+      fs.rmSync(path.join(dirPath, fileName), { force: true });
+    });
+}
+
 export default function deleteSourceMaps() {
-  if (fs.existsSync(webpackPaths.distMainPath))
-    rimrafSync(path.join(webpackPaths.distMainPath, '*.js.map'), {
-      glob: true,
-    });
-  if (fs.existsSync(webpackPaths.distRendererPath))
-    rimrafSync(path.join(webpackPaths.distRendererPath, '*.js.map'), {
-      glob: true,
-    });
+  deleteSourceMapsIn(webpackPaths.distMainPath);
+  deleteSourceMapsIn(webpackPaths.distRendererPath);
 }
